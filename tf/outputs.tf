6e298593bba04a8b1020a14100a62f8700ed8a0f
Julien Corioland <jucoriol@microsoft.com>
2019-12-09 17:21:13 +0100

    upgrade to 0.12 syntax

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,7 +1,8 @@
 output "vnet_name" {
-  value = "${azurerm_virtual_network.main.name}"
+  value = azurerm_virtual_network.main.name
 }
 
 output "resource_group_name" {
-  value = "${azurerm_resource_group.main.name}"
+  value = azurerm_resource_group.main.name
 }
+
